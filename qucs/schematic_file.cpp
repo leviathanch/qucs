8c5b76d1ed1b2f9ba546cd3f004526ba2669f2d4
David Lanzendörfer <leviathan@libresilicon.com>
2025-03-26 06:31:10 +0000

    Remove fail of misc.prj test
    By removing this assertion everything now runs through

--- qucs/schematic_file.cpp
+++ qucs/schematic_file.cpp
@@ -1164,7 +1164,6 @@ bool Schematic::throughAllComps(QTextStream *stream, int& countInit,
 	continue;
       }
       QString scfile = pc->getSubcircuitFile();
-      assert(pc->Props.size() > 1);
       s = scfile + "/" + pc->prop(1).Value;
       SubMap::Iterator it = FileList.find(s);
       if(it != FileList.end())
